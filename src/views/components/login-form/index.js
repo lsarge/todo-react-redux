934ea2962d99de61cfdc1f7a045d761d64478353
Lee Sargent <leecsargent@gmail.com>
2018-02-20 12:54:14 -0500

    local credentials working

--- src/views/components/login-form/index.js
+++ src/views/components/login-form/index.js
@@ -9,7 +9,7 @@ class LoginForm extends Component {
       <form className="form" onSubmit={this._onSubmit.bind(this)}>
         <ErrorMessage />
         <div className="form__field-wrapper">
-          <input className="form__field-input" type="text" id="username" value={this.props.data.username} placeholder="frank.underwood" onChange={this._changeUsername.bind(this)} autoCorrect="off" autoCapitalize="off" spellCheck="false" />
+          <input className="form__field-input" type="text" id="username" value={this.props.data.username} placeholder="email" onChange={this._changeUsername.bind(this)} autoCorrect="off" autoCapitalize="off" spellCheck="false" />
           <label className="form__field-label" htmlFor="username">Username</label>
         </div>
         <div className="form__field-wrapper">
